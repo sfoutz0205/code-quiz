cd209e0f22fbbb0950b87d27e2150300ebe55b2b
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-08-22 22:31:27 -0600

    questions & timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
 var startContainer = document.querySelector("#start-quiz");
-var start = document.querySelector("#start-button");
+var startButton = document.querySelector("#start-btn");
 var quizContainer = document.querySelector("#quiz");
-var question = document.querySelector("#question");
+var questionElement = document.querySelector("#question");
 var choiceA = document.querySelector("#A");
 var choiceB = document.querySelector("#B");
 var choiceC = document.querySelector("#C");
 var choiceD = document.querySelector("#D");
 var scoreContainer = document.querySelector("#score-container");
-var runningQuestionIndex = 0;
+var shuffledQuestions, currentQuestionIndex
 
-// Quiz Questions
 
+// TIMER ELEMENT
+var startTimer = function () {
+    var counter = 60;
+    setInterval(function() {
+    counter--;
+    if(counter >= 0) {
+        time = document.getElementById("count");
+        time.innerHTML = "Time: " + counter;
+    }
+}, 1000);
+};
+
+
+// QUIZ QUESTIONS/ANSWERS
 var questions = [
     {
-        question : "What does HTML stand for?",
+        question : "QUESTION 11111111111111111111?",
         choiceA : "Answer A",
         choiceB : "Answer B",
         choiceC : "Answer C",
@@ -21,7 +34,15 @@ var questions = [
         correct: "A"
     },
     {
-        question : "What does HTML stand for?",
+        question : "QUESTION 22222222222222222?",
+        choiceA : "Answer A",
+        choiceB : "Answer B",
+        choiceC : "Answer C",
+        choiceD : "Answer D",
+        correct: "D"
+    },
+    {
+        question : "QUESTION 3333333333333?",
         choiceA : "Answer A",
         choiceB : "Answer B",
         choiceC : "Answer C",
@@ -29,15 +50,7 @@ var questions = [
         correct: "A"
     },
     {
-        quizQuestion : "What does HTML stand for?",
-        choiceA : "Answer A",
-        choiceB : "Answer B",
-        choiceC : "Answer C",
-        choiceD : "Answer D",
-        correct: "A"
-    },
-    {
-        question : "What does HTML stand for?",
+        question : "QUESTION 444444444444444?",
         choiceA : "Answer A",
         choiceB : "Answer B",
         choiceC : "Answer C",
@@ -45,7 +58,7 @@ var questions = [
         correct: "A"
     },
     { 
-        question : "What does HTML stand for?",
+        question : "QUESTION 5555555555555?",
         choiceA : "Answer A",
         choiceB : "Answer B",
         choiceC : "Answer C",
@@ -54,22 +67,35 @@ var questions = [
     }
 ];
 
-var lastQuestionIndex = questions.length - 1;
-
+// START QUIZ FUNCTION
 var startQuiz = function() {
-    startContainer.style.display = "none";
-    questionRender();
-    quizContainer.style.display = "flex";
+    startTimer();
+    startContainer.style.display = "none"
+    currentQuestionIndex = 0
+    quizContainer.style.display = "flex"
+    setNextQuestion()
 };
 
-var questionRender = function(){
-    var q = questions[runningQuestionIndex];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
+var setNextQuestion = function() {
+    showQuestion([currentQuestionIndex]);
+}
+
+var showQuestion = function() {
+    var q = questions[currentQuestionIndex];
+    question.innerHTML = "<p>" + q.question + "</p>"
+    choiceA.innerHTML = q.choiceA
+    choiceB.innerHTML = q.choiceB
+    choiceC.innerHTML = q.choiceC
+    choiceD.innerHTML = q.choiceD
 };
 
-
-start.addEventListener("click", startQuiz );
+var checkAnswer = function(answer) {
+    if(questions[currentQuestionIndex].correct == answer){
+        currentQuestionIndex++
+        showQuestion()
+    }
+    else {
+        alert("NAH")
+    }
+}
+startButton.addEventListener("click", startQuiz );
